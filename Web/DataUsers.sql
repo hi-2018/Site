313723b2e8a8e85f9af0fd2afe3b04c6c222ddab
yangsc1001 <yangsc1001@gmail.com>
2019-11-16 11:50:35 +0800

    数据库

---
+++ Web/DataUsers.sql
@@ -0,0 +1,12 @@
+﻿Use master;
+go
+create Database DataUsers on
+(name=DataUsers,
+FILENAME='E:\site\Web\App_Data\DataUsers.mdf',
+Size=5mb,maxsize=25mb,filegrowth=1mb
+)
+log on
+(name=DataUsers_log,
+filename='E:\site\Web\App_Data\DataUsers_log.ldf',
+size=3mb,maxsize=25mb,filegrowth=1mb)
+collate Chinese_PRC_CI_AS;
